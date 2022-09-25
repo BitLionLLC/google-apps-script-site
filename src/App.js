e12eb0b838e4d52ad643577d23ce39e1c5eb2eab
Grant Reighard <grant.reighard@gmail.com>
2022-09-25 13:12:26 -0400

    Builds out privacy policy and linking.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import MonthlyExpensesTracker from './components/privacy-policies/monthlyExpensesTracker';
 
+import './App.css'
+
 function App() {
   return (
     <div className="App">
--- src/components/privacy-policies/monthlyExpensesTracker.js
+++ src/components/privacy-policies/monthlyExpensesTracker.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MonthlyExpensesTracker = () => {
     return (
         <div>
             <h1>Monthly Expenses Tracker</h1>
-            <p></p>
+            <p>
+                Monthly Expenses Tracker is a Google Apps Script application, 
+                developed by BitLion, LLC.
+            </p>
+            <p>
+                We don't collect or store your data.
+            </p>
+            <p>
+                We need authorization of one Google API scope,
+            </p>
+            <span className="code">https://www.googleapis.com/auth/spreadsheets</span>
+            <p>
+                which lets us "See, edit, create, and delete all your Google Sheets spreadsheets".
+            </p>
+            <p>
+                In reality, we're only editing the spreadsheet which the scripts are attached to,
+                the Monthly Expenses Tracker template. You can verify this by clicking on the
+                Extensions menu in the template in Google Drive and clicking on Apps Script
+                so you can read the scripts yourself.
+            </p>
+            <p>
+                For instance, the only sheet ever accessed, the active one, is accessed by
+            </p>
+            <span className="code">var activeSht = SpreadsheetApp.getActiveSheet();</span>
+            <p>
+                Only the cells with black backgrounds and white text are being updated by scripts.
+            </p>
+            <p>
+                The scripts are run on Google's servers. You need to have a Google account in order
+                to use the template and run the scripts. Google's use of your data is described in&nbsp;
+                <a href='https://www.google.com/policies/privacy/'>Google's Privacy Policy.</a>
+            </p>
+            <p>
+                <Link to="/">Home</Link>
+            </p>
         </div>
     )
 }
